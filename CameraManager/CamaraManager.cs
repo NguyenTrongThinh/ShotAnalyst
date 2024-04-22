cdfd75b19a6353cdfa96a58b57b3449a05c43844
Thinh Nguyen <nguyentrongthinhit@gmail.com>
2024-04-22 09:00:25 +0700

    Initial Projects

---
+++ CameraManager/CamaraManager.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CameraManager
+{
+    /// <summary>
+    /// Cung cấp các APIs để liệt kê các camera cắm sẵn qua cổng USB trên máy tính.
+    /// Nó cũng cung cấp 1 Event để thông báo camera nào bị rút ra, hoặc cắm thêm vào.
+    /// </summary>
+    public class CamaraManager
+    {
+
+    }
+
+}
